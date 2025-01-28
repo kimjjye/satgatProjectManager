07bd15607bb1fac7ea4b7fa741fff468948ecf8e
DESKTOP-99QRFV8\rkawk <jjye81930@gmail.com>
2025-01-28 17:02:08 +0900

    # feat set up project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>SATGAT</div>
+    </>
+  );
+}
+
+export default App;
---
+++ project-manager-backend/src/test/java/com/satgat/project_manager_backend/ProjectManagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.satgat.project_manager_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
